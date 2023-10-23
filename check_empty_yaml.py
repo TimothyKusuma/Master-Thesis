fed148197081c56fe9569d0962f61394e78a4ae9
TimothyKusuma <s_kusuma@stud.hwr-berlin.de>
2023-10-23 22:04:44 +0200

    new

--- check_empty_yaml.py
+++ check_empty_yaml.py
@@ -1,27 +1,46 @@
 import os
+import re
 
-def check_empty_yaml_files(directory):
+def check_yaml_files(directory):
     # List all files in the specified directory
-    files = os.listdir(directory)
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     
-    # Filter for only .yaml files
-    yaml_files = [f for f in files if f.endswith('.yaml')]
+    # Filter for YAML files with the specified naming pattern
+    yaml_files = [f for f in files if (f.endswith('.yaml') or f.endswith('.yml')) and f.startswith('diagram')]
+    
+    # Sort the YAML files by the numeric portion of their names
+    yaml_files.sort(key=lambda f: int(re.search(r'\d+', f).group()))
     
     # Check each YAML file to see if it's empty
     empty_files = [f for f in yaml_files if os.path.getsize(os.path.join(directory, f)) == 0]
+    non_empty_files = [f for f in yaml_files if os.path.getsize(os.path.join(directory, f)) != 0]
     
-    return empty_files
+    return empty_files, non_empty_files
 
-# Specify the directory to check
-directory = 'chatGPT_results'
+def main():
+    directory = 'chatGPT_results'
+    
+    # Ensure the directory exists
+    if not os.path.exists(directory):
+        print(f"The directory {directory} does not exist.")
+        return
+    
+    # Get lists of empty and non-empty YAML files
+    empty_files, non_empty_files = check_yaml_files(directory)
+    
+    if empty_files:
+        print("Empty YAML files:")
+        for f in empty_files:
+            print(f)
+    else:
+        print("No empty YAML files found.")
+    
+    if non_empty_files:
+        print("\nNon-empty YAML files:")
+        for f in non_empty_files:
+            print(f)
+    else:
+        print("No non-empty YAML files found.")
 
-# Get the list of empty YAML files
-empty_files = check_empty_yaml_files(directory)
-
-# Print the results
-if empty_files:
-    print(f"Empty YAML files in the '{directory}' directory:")
-    for file in empty_files:
-        print(f"- {file}")
-else:
-    print(f"No empty YAML files found in the '{directory}' directory.")
+if __name__ == '__main__':
+    main()
