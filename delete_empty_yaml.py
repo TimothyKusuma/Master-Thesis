daf8c43b816ba5a0295d26478393f36cd62bc0b2
TimothyKusuma <s_kusuma@stud.hwr-berlin.de>
2023-11-06 19:17:55 +0100

    new

---
+++ delete_empty_yaml.py
@@ -0,0 +1,56 @@
+import os
+import re
+
+def check_yaml_files(directory):
+    # List all files in the specified directory
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    
+    # Filter for YAML files with the specified naming pattern
+    yaml_files = [f for f in files if (f.endswith('.yaml') or f.endswith('.yml')) and f.startswith('diagram')]
+    
+    # Sort the YAML files by the numeric portion of their names
+    yaml_files.sort(key=lambda f: int(re.search(r'\d+', f).group()))
+    
+    # Check each YAML file to see if it's empty
+    empty_files = [f for f in yaml_files if os.path.getsize(os.path.join(directory, f)) == 0]
+    non_empty_files = [f for f in yaml_files if os.path.getsize(os.path.join(directory, f)) != 0]
+    
+    return empty_files, non_empty_files
+
+def delete_empty_files(directory, empty_files):
+    # Delete each empty file found in the directory
+    for file in empty_files:
+        file_path = os.path.join(directory, file)
+        os.remove(file_path)
+        print(f"Deleted empty file: {file}")
+
+def main():
+    directory = 'chatGPT_results'
+    
+    # Ensure the directory exists
+    if not os.path.exists(directory):
+        print(f"The directory {directory} does not exist.")
+        return
+    
+    # Get lists of empty and non-empty YAML files
+    empty_files, non_empty_files = check_yaml_files(directory)
+    
+    # Display empty YAML files and delete them
+    if empty_files:
+        print("Empty YAML files:")
+        for f in empty_files:
+            print(f)
+        delete_empty_files(directory, empty_files)
+    else:
+        print("No empty YAML files found to delete.")
+    
+    # Optionally display non-empty YAML files
+    if non_empty_files:
+        print("\nNon-empty YAML files:")
+        for f in non_empty_files:
+            print(f)
+    else:
+        print("No non-empty YAML files found.")
+
+if __name__ == '__main__':
+    main()
